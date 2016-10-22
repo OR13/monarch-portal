f5cac3976e532b24cd524ef5ae193c238a936f53
Orie Steele <orie@ohs.io>
2016-10-22 15:44:16 -0500

    stash truffle import / everything broken

--- app/scripts/pages/truffle.js
+++ app/scripts/pages/truffle.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import i18n from '../utils/i18n.js'
 import { Row, Col } from 'react-bootstrap'
 
+import AccountListContainer from '../AccountList/AccountListContainer'
+
 export default class Truffle extends Component {
   state = {
   };
@@ -21,6 +23,7 @@ export default class Truffle extends Component {
       <Row>
         <Col sm={10} smOffset={1}>
           <h3>{i18n.t('Truffle Demo')}</h3>
+            <AccountListContainer web3={this.props.web3} />
         </Col>
       </Row>
     )
